94e98e2f092ae84993a811e634a5b32b8cdd2a7d
Yi <yp269@njit.edu>
2019-05-08 21:33:05 -0400

    Feature:Write a feature test to check if the register return a 200 status code

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Feature;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+    }
+}
